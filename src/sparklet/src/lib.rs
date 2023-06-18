1238113e3a849d28ae43362bfb1064b11cb9291f
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-06-19 03:50:50 +0900

    Add a function returns its version

--- src/sparklet/src/lib.rs
+++ src/sparklet/src/lib.rs
@@ -1,3 +1,8 @@
-pub fn main() {
-    println!("Hello, world!");
+#[allow(dead_code)]
+fn main() {}
+
+pub fn get_version() -> String {
+    let pkg = env!("CARGO_PKG_NAME");
+    let ver = env!("CARGO_PKG_VERSION");
+    format!("{} v{}", pkg, ver)
 }
